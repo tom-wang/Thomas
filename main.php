9cf7689f18c3b694ef63a08755c0e9ee960a01c9
tomszwang <tom.wang.sz@qq.com>
2017-03-18 00:26:25 +0800

    实现autoload

--- main.php
+++ main.php
@@ -4,6 +4,19 @@
  */
 namespace Thomas;
 
-// 注册autoload
-spl_autoload_register();
-require_once __DIR__ . '/';
+Thomas::init();
+
+class Thomas {
+    /**
+     * 初始化
+     */
+    public static function init() {
+        // 注册autoload
+        spl_autoload_register(function($className) {
+            // 仅处理Thomas命名空间下的类
+            if(0 === strpos($className, 'Thomas\\')) {
+                require_once(__DIR__ . DIRECTORY_SEPARATOR . str_replace(array('Thomas\\', '\\'), array('', DIRECTORY_SEPARATOR), $className) . '.php');
+            }
+        });
+    }
+}
