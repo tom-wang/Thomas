0c894b8cbbfb4927d8d860f822836f8f76e410fc
tomszwang <tom.wang.sz@qq.com>
2017-03-27 23:42:41 +0800

    修改page目录为小写

---
+++ page/HtmlPage.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ */
+namespace Thomas\Page;
+use Thomas\Page\Page;
+
+class HtmlPage extends Page {
+    public function render() {
+        var_dump('hello, tom');
+    }
+}
---
+++ page/JsonPage.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ */
+namespace Thomas\Page;
+use Thomas\Page\Page;
+
+class JsonPage extends Page {
+    public function render() {
+    }
+}
